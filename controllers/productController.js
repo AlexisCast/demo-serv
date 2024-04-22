295362a43fdc5686f9fa9a63827c29e67f2edd0c
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-04-21 18:41:07 -0600

    read documents

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,33 +1,38 @@
 const Product = require('../models/productModel');
 
-exports.checkBody = (req, res, next) => {
-  const { name, price } = req.body;
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
 
-  if (!name || !price) {
-    return res.status(400).json({
-      msg: `Missing name or price`,
+    res.status(200).json({
+      results: products.length,
+      requestedAt: req.requestTime,
+      data: {
+        products: products,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      msg: 'Not able to get products',
     });
   }
-
-  next();
 };
 
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({
-    // results: products.length,
-    requestedAt: req.requestTime,
-    data: {
-      // products: products,
-    },
-  });
-};
+exports.getProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
 
-exports.getProduct = (req, res) => {
-  res.status(200).json({
-    data: {
-      // product: req.product,
-    },
-  });
+    res.status(200).json({
+      data: {
+        product: product,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      msg: 'Product not found!',
+    });
+  }
 };
 
 exports.createProduct = async (req, res) => {
