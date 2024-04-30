e3127672d7eab496d5c191cfbcb33f4b862aad5e
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-04-30 15:36:32 -0600

    refactor global errors

---
+++ utils/appError.js
@@ -0,0 +1,13 @@
+function appError(message, statusCode) {
+  const error = new Error(message);
+
+  error.statusCode = statusCode;
+  error.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+  error.isOperational = true;
+
+  Error.captureStackTrace(error, appError);
+
+  return error;
+}
+
+module.exports = { appError };
