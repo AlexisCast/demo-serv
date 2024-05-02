0c939555b5ad412d8cae5f4ea5f7cf2e9b03567f
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-05-02 03:33:31 -0600

    implement users log in

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,15 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    msg: 'This route is not yet defined',
+const User = require('../models/userModel');
+
+exports.getAllUsers = async (req, res) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    // results: totalProtducts,
+    shown: users.length,
+    requestedAt: req.requestTime,
+    data: {
+      users: users,
+    },
   });
 };
 
